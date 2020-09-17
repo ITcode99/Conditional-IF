9db320329465fc6776bbf6bcbdfc0027405a7a70
ITCODE <70245061+ITcode99@users.noreply.github.com>
2020-09-17 08:18:26 +0700

    Add files via upload

---
+++ conditionalif/src/conditionalif/Conditionalif.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package conditionalif;
+
+/**
+ *
+ * @author Adi Arput
+ */
+public class Conditionalif {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int a = 3;
+            System.out.println("Start");
+            
+            if (a == 1){
+                System.out.println("1 adalah bilangan Ganjil");
+            }else if(a == 2){
+                System.out.println("2 adalah bilangan Genap");
+            }else if(a == 3){
+                System.out.println("3 adalah bilangan Ganjil");
+            }else if(a == 4){
+                System.out.println("4 adalah bilangan Genap");
+            }else if(a == 5){
+                System.out.println("5 adalah bilangan Ganjil");
+            }else if(a == 6){
+                System.out.println("6 adalah bilangan Genap");
+            }else if(a == 7){
+                System.out.println("7 adalah bilangan Ganjil");
+            }else if(a == 8){
+                System.out.println("8 adalah bilangan Genap");
+            }else if(a == 9){
+                System.out.println("9 adalah bilangan Ganjil");
+            }else if(a == 0){
+                System.out.println("0 adalah bilangan Default");
+            }
+    }
+    
+}
